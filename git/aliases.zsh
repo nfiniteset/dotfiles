e33618446444b0afef02e6e5e1f830e98e7138c2
Albert Llop <mrsimo@gmail.com>
2010-08-21 17:21:06 +0200

    I like different git aliases, and also added my gitconfig, but ignoring it in git

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,10 +1,11 @@
-alias gl='git pull --rebase --prune'
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias gpull='git pull --rebase --prune'
+alias gl="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
 alias gd='git diff'
 alias gc='git commit'
 alias gca='git commit -a'
 alias gco='git checkout'
 alias gb='git branch'
-alias gs='git status -sb'
+alias gs='git status'
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+alias ga="git add"
