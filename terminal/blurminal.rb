fc394571b2075d5435f7ab292f5f6901cc8042e1
Albert Llop <mrsimo@gmail.com>
2010-08-26 17:48:27 +0200

    Installation of Blurminal and TabSwitching for Terminal

--- terminal/blurminal.rb
+++ terminal/blurminal.rb
@@ -1,21 +1,21 @@
 require 'geoffrey'
 Geoffrey.package do
-  url "http://github.com/dabeeeenster/terminaltabswitching/raw/master/TerminalTabSwitching.bundle.zip"
-
-  options :file => "TerminalTabSwitching.bundle"
+  url "http://github.com/albertllop/Blurminal/raw/master/Blurminal.bundle.zip"
+  options :file => "Blurminal.bundle"
 
   def install
 
     plugins_dir = "#{ENV["HOME"]}/Library/Application Support/SIMBL/Plugins"
 
     unless File.exists?("#{plugins_dir}/#{file_to_install}")
-      puts "TerminalTabSwitching bundle already installed"
+      puts "Blurminal bundle already installed"
     else
-      puts "Installing TerminalTabSwitching bundle"
+      puts "Installing Blurminal bundle"
       FileUtils.mkdir_p plugins_dir
       FileUtils.mv file_to_install, plugins_dir
       puts "You only have to reload Terminal.app now"
     end
 
   end
+
 end
